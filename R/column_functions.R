a1c5b8f0c6e39695e3c61facc0eb514111b79414
Francesca Bryden <francesca.bryden@dft.gov.uk>
2022-05-24 17:08:58 +0100

    Removed ggplot dependency and made applicable to plotly by swapping check for ggplot objects

--- R/column_functions.R
+++ R/column_functions.R
@@ -5,13 +5,19 @@
 #' @param left string, text content to appear in the smaller left column
 #' @param right any R studio object (chart, table, text) to appear in the larger right column
 #' @title Create a column structure for a slide with a large right column (75\% width) and small left column (25\% width)
-#' @importFrom knitr asis_output
+#' @importFrom knitr asis_output opts_current
 #' @examples 
 #' uneven_cols("This is text", plot(mtcars))
 uneven_cols <- function(left, right){
   
-  if(ggplot2::is.ggplot(right)){
-    right <- print(right)[0]
+  if(knitr::opts_current$get("fig.show") != "hide"){
+    stop("Chunk option fig.show must be set to 'hide'")
+  }
+  
+  if("ggplot" %in% class(left)|"plotly" %in% class(left)){
+    right <- convert_ggplot(right, img_no)
+    img_no <- img_no + 1
+    
   }
   
   knitr::asis_output(
@@ -36,7 +42,6 @@ uneven_cols <- function(left, right){
 #' @param right content to appear in the right column
 #' @title Create a column structure for a slide with equal right and left columns
 #' @importFrom knitr asis_output opts_current
-#' @importFrom ggplot2 is.ggplot
 #' @examples 
 #' even_cols("This is text", plot(mtcars))
 even_cols <- function(left, right){
@@ -48,12 +53,12 @@ even_cols <- function(left, right){
   ##Set starting image number
   img_no <- 1
   
-  if(ggplot2::is.ggplot(left)){
+  if("ggplot" %in% class(left)|"plotly" %in% class(left)){
     left <- convert_ggplot(left, img_no)
     img_no <- img_no + 1 #Imcrement image number
   }
   
-  if(ggplot2::is.ggplot(right)){
+  if("ggplot" %in% class(left)|"plotly" %in% class(left)){
     right <- convert_ggplot(right, img_no)
     img_no <- img_no + 1
     
